6d41cb35372f5dc7fd8bddfe74a1216aa771658a
Segura <segura-deluna@mail.ru>
2020-07-05 16:30:25 +0300

    page faq mobile

--- js/slider.js
+++ js/slider.js
@@ -9,7 +9,11 @@ const faqSwiper = new Swiper('.faq__slider-container', {
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
-	}
+	},
+	scrollbar: {
+		el: '.swiper-scrollbar',
+		draggable: true,
+	},
 });
 
 
