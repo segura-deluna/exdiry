622f3eba2beee3b84d538c4d50da08ff921e22e3
Segura <segura-deluna@mail.ru>
2020-07-12 00:00:55 +0300

    added burger in personal room

--- js/burger.js
+++ js/burger.js
@@ -1,7 +1,13 @@
 
 const nameText = document.querySelector('.name-text'),
-	nameMenu = document.querySelector('.name-menu');
+	nameMenu = document.querySelector('.name-menu'),
+	personalMenu = document.querySelector('.personal__menu'),
+	burger = document.querySelector('.menu-burger');
 
-nameText.addEventListener('click', function () {
+nameText.addEventListener('click', () => {
 	nameMenu.classList.toggle('active');
+});
+
+burger.addEventListener('click', () => {
+	personalMenu.classList.toggle('active');
 });
