79742fd4e1df06e8f9390d174a8a57e2c8500609
Segura <segura-deluna@mail.ru>
2020-07-12 16:42:20 +0300

    added mobile menu in personal room

--- js/burger.js
+++ js/burger.js
@@ -1,13 +1,18 @@
 
-const nameText = document.querySelector('.name-text'),
+const nameText = document.querySelector('.personal-header__name'),
 	nameMenu = document.querySelector('.name-menu'),
 	personalMenu = document.querySelector('.personal__menu'),
-	burger = document.querySelector('.menu-burger');
+	burger = document.querySelector('.menu-burger'),
+	menuClose = document.querySelector('.personal__menu-header-close');
 
 nameText.addEventListener('click', () => {
 	nameMenu.classList.toggle('active');
 });
 
 burger.addEventListener('click', () => {
+	personalMenu.classList.toggle('active');
+});
+
+menuClose.addEventListener('click', () => {
 	personalMenu.classList.toggle('active');
 });
